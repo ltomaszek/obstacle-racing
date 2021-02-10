018812883894957b88af26e9f26215dfc8a3c1ba
Lukasz Tomaszek <l.tomaszekk@gmail.com>
2021-02-10 23:40:55 +0100

    Added/updated collision management for multiplayer/singleplayer

--- ltcode/collision/collision.py
+++ ltcode/collision/collision.py
@@ -1,17 +1,22 @@
 from car.car import Car
 from obstacle.obstacle import Obstacle
 
-def is_collision_with_any_obstacle(car, obstacle_list) -> bool:
+
+def is_collision_with_any_obstacle(car, obstacle_list, x_car_shift=0) -> bool:
     for obstacle in obstacle_list:
-        if is_collision(car, obstacle):
+        if is_collision(car, obstacle, x_car_shift):
             return True
     return False
 
-def is_collision(car, obstacle:Obstacle):
-    return can_collide_Y(car, obstacle) and can_collide_X(car, obstacle)
 
-def can_collide_Y(car:Car, obstacle:Obstacle):
+def is_collision(car, obstacle:Obstacle, x_car_shift):
+    return _can_collide_Y(car, obstacle) and _can_collide_X(car, obstacle, x_car_shift)
+
+
+def _can_collide_Y(car:Car, obstacle:Obstacle):
     return car.get_front_y() <= obstacle.get_down_y() and car.get_back_y() >= obstacle.get_up_y()
 
-def can_collide_X(car:Car, obstacle:Obstacle):
-    return car.get_left_x() <= obstacle.get_right_x() and car.get_right_x() >= obstacle.get_left_x()
+
+def _can_collide_X(car:Car, obstacle:Obstacle, x_car_shift):
+    return car.get_left_x() + x_car_shift <= obstacle.get_right_x() \
+           and car.get_right_x() + x_car_shift >= obstacle.get_left_x()
