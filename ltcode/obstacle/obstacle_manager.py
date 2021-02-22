6349410272840424fae59560e7c7e96cdfb1bb2e
Lukasz Tomaszek <l.tomaszekk@gmail.com>
2021-02-22 21:57:13 +0100

    Added obstacle manager for multi-player

--- ltcode/obstacle/obstacle_manager.py
+++ ltcode/obstacle/obstacle_manager.py
@@ -1,16 +1,18 @@
+import pygame as pg
+import obstacle.obstacle as obstacle
 from obstacle.obstacle import Obstacle
+from settings import settings
+
 
 class ObstacleManager:
+    def __init__(self):
+        self.obstacle_list = []
 
-    def __init__(self, obstacle_list=[]):
-        self.obstacle_list = obstacle_list
-
-    def update_all(self, surface):
+    def update(self, surface):
         # counts obstacle that hit the end of screen
         count_hit_end = 0
         for obstacle in self.obstacle_list:
             count_hit_end += obstacle.update()
-            obstacle.draw(surface)
         return count_hit_end
 
     def generate_random_obstacles(self, num_obstacles):
@@ -25,3 +27,6 @@ class ObstacleManager:
         self.obstacle_list.append(Obstacle())
         for obstacle in self.obstacle_list:
             obstacle.speed += 0.5
+
+    def draw(self, surface, obstacle, x_shift=0):
+        pg.draw.rect(surface, obstacle.color, [obstacle.x + x_shift, obstacle.y, obstacle.width, obstacle.height])
